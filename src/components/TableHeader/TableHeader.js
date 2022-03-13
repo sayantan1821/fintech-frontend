926782e29af6753de30f2f9b09b27911bc1cc75e
sayantan1821 <ksayantan1823@gmail.com>
2022-03-13 23:56:03 +0530

    add predict, analytics, advance search button

--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -1,21 +1,47 @@
 import React from "react";
 import { Checkbox, Button, ButtonGroup } from "@material-ui/core";
-const TableHeader = ({deleteRows}) => {
+const TableHeader = ({ deleteRows }) => {
   const deletec = (e) => {
     e.preventDefault();
-    deleteRows()
-  }
+    deleteRows();
+  };
   return (
-    <div style={{ height: "8vh" }}>
-      <ButtonGroup
-        variant="contained"
-        color="primary"
-        aria-label="contained primary button group"
-      >
-        <Button>ADD</Button>
-        <Button>EDIT</Button>
-        <Button onClick={(e) => {deletec(e)}}>DELETE</Button>
-      </ButtonGroup>
+    <div
+      style={{
+        height: "8vh",
+        display: "flex",
+        justifyContent: "space-between",
+        margin: "0 30px",
+      }}
+    >
+      <div>
+        <ButtonGroup
+          variant="contained"
+          color="primary"
+          aria-label="contained primary button group"
+        >
+          <Button>PREDICT</Button>
+          <Button>ANALYTICS VIEW</Button>
+          <Button onClick={(e) => {}}>ADVANCE SEARCH</Button>
+        </ButtonGroup>
+      </div>
+      <div>
+        <ButtonGroup
+          variant="contained"
+          color="primary"
+          aria-label="contained primary button group"
+        >
+          <Button>ADD</Button>
+          <Button>EDIT</Button>
+          <Button
+            onClick={(e) => {
+              deletec(e);
+            }}
+          >
+            DELETE
+          </Button>
+        </ButtonGroup>
+      </div>
     </div>
   );
 };
